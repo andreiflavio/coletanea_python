a5c76209aeb51efd588b0d07b00bc82088f151f2
flaviosilva <andrei.flavio@hotmail.com>
2020-04-12 18:59:12 -0300

    Adicionado tratamento para usuário informar tipo das imagens contidas no diretório base para geração do pdf

--- convert_png_pdf/pngtopdf.py
+++ convert_png_pdf/pngtopdf.py
@@ -13,9 +13,9 @@ pdf = FPDF(orientation='P', format='A4')
 logging.basicConfig(level=logging.INFO)
 
 
-def main(folder_with_files=None, filename="NewPDF"):
+def main(folder_with_files=None, filename="NewPDF", type_file="png"):
     files_error.clear()
-    create_pdf_from_png_files(folder_with_files, filename)
+    create_pdf_from_png_files(folder_with_files, filename, type_file)
     if len(files_error) > 0:
         run_image_magick = input("There is some problems about pdf created.\
             Do you want to solve that problems (y/n)?")
@@ -23,13 +23,13 @@ def main(folder_with_files=None, filename="NewPDF"):
             logging.info("PDF will be recreated.")
             for item in files_error:
                 os.system("convert -interlace none %s %s" % (item, item))
-            main(folder_with_files, filename)
+            main(folder_with_files, filename, type_file)
 
 
-def create_pdf_from_png_files(folder_with_files, filename):
+def create_pdf_from_png_files(folder_with_files, filename, type_file):
     count_files_folder = len(os.listdir(folder_with_files))
     for i in range(1, count_files_folder + 1):
-        path_file = folder_with_files + "/%d.png" % i
+        path_file = folder_with_files + "/%d.%s" % (i, type_file)
         # TODO - trocar este log info por um spinner
         logging.info("PNG %s file will be included on pdf file." % path_file)
         try:
@@ -46,4 +46,5 @@ def create_pdf_from_png_files(folder_with_files, filename):
 if __name__ == "__main__":
     path = input("Where are png files?")
     filename = input("What is the name for pdf file you desired?")
-    main(path, filename)
+    type_file = input("What is the type of images (PNG is default)?")
+    main(path, filename, type_file)
